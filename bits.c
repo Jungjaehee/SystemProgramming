1cdbdd5a79235b70f7604d5c56ccaf080338f80e
정재희 <wjfwogml5890@naver.com>
2019-10-07 18:29:22 +0900

    LAB1

---
+++ bits.c
@@ -0,0 +1,426 @@
+/* 
+ * CS:APP Data Lab 
+ * 
+ * <Please put your name and userid here>
+ * 201701594 Jung Jaehee
+ * bits.c - Source file with your solutions to the Lab.
+ *          This is the file you will hand in to your instructor.
+ *
+ * WARNING: Do not include the <stdio.h> header; it confuses the dlc
+ * compiler. You can still use printf for debugging without including
+ * <stdio.h>, although you might get a compiler warning. In general,
+ * it's not good practice to ignore compiler warnings, but in this
+ * case it's OK.  
+ */
+
+#if 0
+/*
+ * Instructions to Students:
+ *
+ * STEP 1: Read the following instructions carefully.
+ */
+
+You will provide your solution to the Data Lab by
+editing the collection of functions in this source file.
+
+INTEGER CODING RULES:
+ 
+  Replace the "return" statement in each function with one
+  or more lines of C code that implements the function. Your code 
+  must conform to the following style:
+ 
+  int Funct(arg1, arg2, ...) {
+      /* brief description of how your implementation works */
+      int var1 = Expr1;
+      ...
+      int varM = ExprM;
+
+      varJ = ExprJ;
+      ...
+      varN = ExprN;
+      return ExprR;
+  }
+
+  Each "Expr" is an expression using ONLY the following:
+  1. Integer constants 0 through 255 (0xFF), inclusive. You are
+      not allowed to use big constants such as 0xffffffff.
+  2. Function arguments and local variables (no global variables).
+  3. Unary integer operations ! ~
+  4. Binary integer operations & ^ | + << >>
+    
+  Some of the problems restrict the set of allowed operators even further.
+  Each "Expr" may consist of multiple operators. You are not restricted to
+  one operator per line.
+
+  You are expressly forbidden to:
+  1. Use any control constructs such as if, do, while, for, switch, etc.
+  2. Define or use any macros.
+  3. Define any additional functions in this file.
+  4. Call any functions.
+  5. Use any other operations, such as &&, ||, -, or ?:
+  6. Use any form of casting.
+  7. Use any data type other than int.  This implies that you
+     cannot use arrays, structs, or unions.
+
+ 
+  You may assume that your machine:
+  1. Uses 2s complement, 32-bit representations of integers.
+  2. Performs right shifts arithmetically.
+  3. Has unpredictable behavior when shifting an integer by more
+     than the word size.
+
+EXAMPLES OF ACCEPTABLE CODING STYLE:
+  /*
+   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+   */
+  int pow2plus1(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     return (1 << x) + 1;
+  }
+
+  /*
+   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+   */
+  int pow2plus4(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     int result = (1 << x);
+     result += 4;
+     return result;
+  }
+
+FLOATING POINT CODING RULES
+
+For the problems that require you to implent floating-point operations,
+the coding rules are less strict.  You are allowed to use looping and
+conditional control.  You are allowed to use both ints and unsigneds.
+You can use arbitrary integer and unsigned constants.
+
+You are expressly forbidden to:
+  1. Define or use any macros.
+  2. Define any additional functions in this file.
+  3. Call any functions.
+  4. Use any form of casting.
+  5. Use any data type other than int or unsigned.  This means that you
+     cannot use arrays, structs, or unions.
+  6. Use any floating point data types, operations, or constants.
+
+
+NOTES:
+  1. Use the dlc (data lab checker) compiler (described in the handout) to 
+     check the legality of your solutions.
+  2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
+     that you are allowed to use for your implementation of the function. 
+     The max operator count is checked by dlc. Note that '=' is not 
+     counted; you may use as many of these as you want without penalty.
+  3. Use the btest test harness to check your functions for correctness.
+  4. Use the BDD checker to formally verify your functions
+  5. The maximum number of ops for each function is given in the
+     header comment for each function. If there are any inconsistencies 
+     between the maximum ops in the writeup and in this file, consider
+     this file the authoritative source.
+
+/*
+ * STEP 2: Modify the following functions according the coding rules.
+ * 
+ *   IMPORTANT. TO AVOID GRADING SURPRISES:
+ *   1. Use the dlc compiler to check that your solutions conform
+ *      to the coding rules.
+ *   2. Use the BDD checker to formally verify that your solutions produce 
+ *      the correct answers.
+ */
+
+
+#endif
+/* Copyright (C) 1991-2016 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses Unicode 8.0.0.  Version 8.0 of the Unicode Standard is
+   synchronized with ISO/IEC 10646:2014, plus Amendment 1 (published
+   2015-05-15).  */
+/* We do not support C11 <threads.h>.  */
+/* 
+ * upperBits - pads n upper bits with 1's
+ *  You may assume 0 <= n <= 32
+ *  Example: upperBits(4) = 0xF0000000
+ *  Legal ops: ! ~ & ^ | + << >>
+ *  Max ops: 10
+ *  Rating: 1
+ */
+int upperBits(int n) {
+    return (!n)+(~0<<(33+(~n)));
+}
+/* 
+ * byteSwap - swaps the nth byte and the mth byte
+ *  Examples: byteSwap(0x12345678, 1, 3) = 0x56341278
+ *            byteSwap(0xDEADBEEF, 0, 2) = 0xDEEFBEAD
+ *  You may assume that 0 <= n <= 3, 0 <= m <= 3
+ *  Legal ops: ! ~ & ^ | + << >>
+ *  Max ops: 25
+ *  Rating: 2
+ */
+int byteSwap(int x, int n, int m) {
+    int a = n << 3;
+    int b = m << 3;
+    int res1 = 0xff << b;
+    int res2 = 0xff << a;
+    int tmp=x&~(res1|res2);
+    return tmp | (((x&res1) >> b)&0xff) << a | (((x&res2) >> a)&0xff) << b;
+}
+/* 
+ * rotateLeft - Rotate x to the left by n
+ *   Can assume that 0 <= n <= 31
+ *   Examples: rotateLeft(0x87654321,4) = 0x76543218
+ *   Legal ops: ~ & ^ | + << >> !
+ *   Max ops: 25
+ *   Rating: 3 
+ */
+int rotateLeft(int x, int n) {
+    return  (x >> 1 >> (32 + ~n)) & ~(~(1 << 32) << n) | (x << n);
+}
+/* howManyBits - return the minimum number of bits required to represent x in
+ *             two's complement
+ *  Examples: howManyBits(12) = 5
+ *            howManyBits(298) = 10
+ *            howManyBits(-5) = 4
+ *            howManyBits(0)  = 1
+ *            howManyBits(-1) = 1
+ *            howManyBits(0x80000000) = 32
+ *  Legal ops: ! ~ & ^ | + << >>
+ *  Max ops: 90
+ *  Rating: 4
+ */
+int howManyBits(int x) {
+	int b_16, b_8, b_4, b_2, b_1, b_0, sign = x >> 31;
+	x = (sign & ~x) | (~sign & x);
+	b_16 = !!(x >> 16) << 4;
+	x = x >> b_16;
+	b_8 = !!(x >> 8) << 3;
+	x = x >> b_8;
+	b_4 = !!(x >> 4) << 2;
+	x = x >> b_4;
+	b_2 = !!(x >> 2) << 1;
+	x = x >> b_2;
+	b_1 = !!(x >> 1);
+	x = x >> b_1;
+	b_0 = x;
+	return b_16 + b_8 + b_4 + b_2 + b_1 + b_0 + 1;
+}
+/*
+ * leftBitCount - returns count of number of consective 1's in
+ *     left-hand (most significant) end of word.
+ *   Examples: leftBitCount(-1) = 32, leftBitCount(0xFFF0F0F0) = 12
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 50
+ *   Rating: 4
+ */
+int leftBitCount(int x) {
+	int tmp = x;
+	int res;
+	int a;
+	res = !(~(tmp >> 16)) << 4; 
+	tmp = tmp << res;
+	a = !(~(tmp >> 24)) << 3;
+	tmp = tmp << a;
+	res = res | a;
+	a = !(~(tmp >> 28)) << 2;
+	tmp = tmp << a;
+	res = res | a;
+	a= !(~(tmp >> 30)) << 1;
+	tmp = tmp << a;
+	res= res|a;
+	res = res ^ (1 & ((tmp >> 31)));
+	return res + !(~x);
+}
+/*
+ * isTmin - returns 1 if x is the minimum, two's complement number,
+ *     and 0 otherwise 
+ *   Legal ops: ! ~ & ^ | +
+ *   Max ops: 10
+ *   Rating: 1
+ */
+int isTmin(int x) {
+    return !!x&!(x+x);
+}
+/* 
+ * fitsBits - return 1 if x can be represented as an 
+ *  n-bit, two's complement integer.
+ *   1 <= n <= 32
+ *   Examples: fitsBits(5,3) = 0, fitsBits(-4,3) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 2
+ */
+int fitsBits(int x, int n) {
+    int tmp=33+~n;
+    return !(x^((x<<tmp)>>tmp));
+}
+/* 
+ * sign - return 1 if positive, 0 if zero, and -1 if negative
+ *  Examples: sign(130) = 1
+ *            sign(-23) = -1
+ *  Legal ops: ! ~ & ^ | + << >>
+ *  Max ops: 10
+ *  Rating: 2
+ */
+int sign(int x) {
+    return (x>>31)|(!!x);
+}
+/* 
+ * isNotEqual - return 0 if x == y, and 1 otherwise 
+ *   Examples: isNotEqual(5,5) = 0, isNotEqual(4,5) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 2
+ */
+int isNotEqual(int x, int y) {
+  return !!(x^y);
+}
+/*
+ * multFiveEighths - multiplies by 5/8 rounding toward 0.
+ *   Should exactly duplicate effect of C expression (x*5/8),
+ *   including overflow behavior.
+ *   Examples: multFiveEighths(77) = 48
+ *             multFiveEighths(-22) = -13
+ *             multFiveEighths(1073741824) = 13421728 (overflow)
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 3
+ */
+int multFiveEighths(int x) {
+    int neg=x>>31;
+    int res=(x<<2)+x+(neg&7);
+    return res>>3;
+}
+/* 
+ * isAsciiDigit - return 1 if 0x30 <= x <= 0x39 (ASCII codes for characters '0' to '9')
+ *   Example: isAsciiDigit(0x35) = 1.
+ *            isAsciiDigit(0x3a) = 0.
+ *            isAsciiDigit(0x05) = 0.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 3
+ */
+int isAsciiDigit(int x) {
+    int x_2com=~x+1; 
+    return !!((0x2f+x_2com)>>31)&!((0x39+x_2com)>>31);
+}
+/*
+ * isPower2 - returns 1 if x is a power of 2, and 0 otherwise
+ *   Examples: isPower2(5) = 0, isPower2(8) = 1, isPower2(0) = 0
+ *   Note that no negative number is a power of 2.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 4
+ */
+int isPower2(int x) {
+  return (!!x)&!(x>>31)&!((x+~1+1)&x);
+}
+/* 
+ * float_abs - Return bit-level equivalent of absolute value of f for
+ *   floating point argument f.
+ *   Both the argument and result are passed as unsigned int's, but
+ *   they are to be interpreted as the bit-level representations of
+ *   single-precision floating point values.
+ *   When argument is NaN, return argument..
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 10
+ *   Rating: 2
+ */
+unsigned float_abs(unsigned uf) {
+    unsigned a=~(1<<31);
+    unsigned res=a&uf;
+    if(res>(0xff<<23))
+	return uf;
+    return res;
+}
+/* 
+ * float_f2i - Return bit-level equivalent of expression (int) f
+ *   for floating point argument f.
+ *   Argument is passed as unsigned int, but
+ *   it is to be interpreted as the bit-level representation of a
+ *   single-precision floating point value.
+ *   Anything out of range (including NaN and infinity) should return
+ *   0x80000000u.
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 30
+ *   Rating: 4
+ */
+int float_f2i(unsigned uf) {
+    unsigned sign = uf>>31;
+    unsigned exp = (uf>>23)&0xff;
+    unsigned frac = uf&(~(0xff<<24)>>1);
+    unsigned res = frac;
+    if(exp == 0xff)
+	return 0x80000000u;
+    if(exp<127)
+	return 0x0;
+    exp-=127;
+    if(exp>32)
+	return 0x80000000u;
+    if(exp>22)
+	res=frac<<(exp-23);
+    else 
+	res=frac>>(23-exp);
+    res+=1<<exp;
+    if(sign)
+	res=-res;
+    return res;
+}
+/* 
+ * float_half - Return bit-level equivalent of expression 0.5*f for
+ *   floating point argument f.
+ *   Both the argument and result are passed as unsigned int's, but
+ *   they are to be interpreted as the bit-level representation of
+ *   single-precision floating point values.
+ *   When argument is NaN, return argument
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 30
+ *   Rating: 4
+ */
+unsigned float_half(unsigned uf){
+	unsigned sign = uf >> 31;
+	unsigned mask = ~(0xff << 24) >> 1;
+	unsigned exp = uf>>23 & 0xff;
+	unsigned frac = uf & mask;
+	if (exp == 0xff)
+		return uf;
+	else if (exp>1) 
+		exp -=1;
+	else {
+		if (exp == 1) {
+			if (frac != mask)
+				exp = 0;
+			frac |= 0x800000;
+		}
+		if ((frac & 3) == 3)
+			frac += 1;
+		frac = (frac >> 1) & mask;
+	}
+	return sign << 31 | exp << 23 | frac;
+}
